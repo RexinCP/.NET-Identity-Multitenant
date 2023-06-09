d1ad873384ace0584f99e6363e554880f150b170
Ryan Wilson <rexincp@gmail.com>
2023-06-09 17:37:28 +1000

    Added projects and core setup

---
+++ Infrastructure/Services/Identity/UserStoreService.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Infrastructure.Services.Identity;
+
+public class UserStoreService
+{
+    public UserStoreService()
+    {
+        throw new NotImplementedException();
+    }
+}
